20ea8998be08e5da32cba49f0c14705941a7dcb9
Edoukou Aman <amanedoukou@gmail.com>
2023-04-13 11:03:11 +0200

    Fonction Afficher toutes les infos de la BD

--- src/main/java/fr/sdv/b32223/App.java
+++ src/main/java/fr/sdv/b32223/App.java
@@ -3,6 +3,7 @@ package fr.sdv.b32223;
 import fr.sdv.b32223.entities.Livre;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -18,13 +19,13 @@ public class App {
 
         em.getTransaction().begin();
 
-        /*
+/*
         //Ajout
         Livre livAjout = new Livre();
         livAjout.setAuteur("Kevin Aman");
         livAjout.setTitre("Les frasques de Kevin");
         em.persist(livAjout);
-         */
+
 
         //Modifier
         Livre livModif = em.find(Livre.class, 5);
@@ -33,22 +34,29 @@ public class App {
         livModif.setAuteur("1001 recettes de Cuisine");
         em.persist(livModif);
 
+*/
+       /* //Extraire en fonction de l'auteur
+        String query = "SELECT liv FROM Livre liv WHERE liv.auteur=:auteur";
+        List<Livre> livres = em.createNamedQuery(query, Livre.class).setParameter("auteur", "Léon Tolstoï").getResultList();
+        for(Livre livre:livres) {
+            System.out.println(livre.getTitre()+", "+ livre.getAuteur());
+        }
+*/
 
         //Extraire en fonction du titre
 
 
-        //Extraire en fonction de l'auteur
-
-
         //Supprimer
-        Livre livSupp = em.find(Livre.class, 6);
-        em.remove(livSupp);
+        //Livre livSupp = em.find(Livre.class, 6);
+        //em.remove(livSupp);
 
 
         //Tout afficher
-
-
-
+        List<Livre> livreAff = null;
+        livreAff = em.createNativeQuery("select * from Livre", Livre.class).getResultList();
+        for(Livre livre:livreAff) {
+            System.out.println("Livre n°"+livre.getId()+" Titre : "+livre.getTitre()+", Auteur : "+ livre.getAuteur());
+        }
 
         em.getTransaction().commit();
 
--- src/main/java/fr/sdv/b32223/entities/Livre.java
+++ src/main/java/fr/sdv/b32223/entities/Livre.java
@@ -19,7 +19,6 @@ public class Livre {
 
     //constructeur
     public Livre() {
-
     }
 
     @Override
